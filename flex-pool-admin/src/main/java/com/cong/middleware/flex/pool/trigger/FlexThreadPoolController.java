5acabdcf2279962c753b43630b6b0bfd015f74ed
lhccong <liuhuaicong@zhut.cn>
2024-08-30 16:47:46 +0800

    feat：新增动态线程池管理端

---
+++ flex-pool-admin/src/main/java/com/cong/middleware/flex/pool/trigger/FlexThreadPoolController.java
@@ -0,0 +1,103 @@
+package com.cong.middleware.flex.pool.trigger;
+
+import com.alibaba.fastjson.JSON;
+import com.cong.middleware.flex.pool.sdk.domain.model.entity.ThreadPoolConfigEntity;
+import com.cong.middleware.flex.pool.types.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RList;
+import org.redisson.api.RTopic;
+import org.redisson.api.RedissonClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Slf4j
+@RestController()
+@CrossOrigin("*")
+@RequestMapping("/api/v1/flex/thread/pool/")
+public class FlexThreadPoolController {
+
+    @Resource
+    public RedissonClient redissonClient;
+
+    /**
+     * 查询线程池数据
+     * curl --request GET  --url http://localhost:8089/api/v1/flex/thread/pool/query_thread_pool_list
+     */
+    @GetMapping("query_thread_pool_list")
+    public Response<List<ThreadPoolConfigEntity>> queryThreadPoolList() {
+        try {
+            RList<ThreadPoolConfigEntity> cacheList = redissonClient.getList("THREAD_POOL_CONFIG_LIST_KEY");
+            return Response.<List<ThreadPoolConfigEntity>>builder()
+                    .code(Response.Code.SUCCESS.getCode())
+                    .info(Response.Code.SUCCESS.getInfo())
+                    .data(cacheList.readAll())
+                    .build();
+        } catch (Exception e) {
+            log.error("查询线程池数据异常", e);
+            return Response.<List<ThreadPoolConfigEntity>>builder()
+                    .code(Response.Code.UN_ERROR.getCode())
+                    .info(Response.Code.UN_ERROR.getInfo())
+                    .build();
+        }
+    }
+
+    /**
+     * 查询线程池配置
+     * curl --request GET \
+     * --url '<a href="http://localhost:8089/api/v1/flex/thread/pool/query_thread_pool_config?appName=flex-thread-pool-test-app&threadPoolName=threadPoolExecutor">...</a>'
+     */
+    @GetMapping("query_thread_pool_config")
+    public Response<ThreadPoolConfigEntity> queryThreadPoolConfig(@RequestParam String appName, @RequestParam String threadPoolName) {
+        try {
+            String cacheKey = "THREAD_POOL_CONFIG_PARAMETER_LIST_KEY" + "_" + appName + "_" + threadPoolName;
+            ThreadPoolConfigEntity threadPoolConfigEntity = redissonClient.<ThreadPoolConfigEntity>getBucket(cacheKey).get();
+            return Response.<ThreadPoolConfigEntity>builder()
+                    .code(Response.Code.SUCCESS.getCode())
+                    .info(Response.Code.SUCCESS.getInfo())
+                    .data(threadPoolConfigEntity)
+                    .build();
+        } catch (Exception e) {
+            log.error("查询线程池配置异常", e);
+            return Response.<ThreadPoolConfigEntity>builder()
+                    .code(Response.Code.UN_ERROR.getCode())
+                    .info(Response.Code.UN_ERROR.getInfo())
+                    .build();
+        }
+    }
+
+    /**
+     * 修改线程池配置
+     * curl --request POST --url http://localhost:8089/api/v1/flex/thread/pool/update_thread_pool_config
+     * --header 'content-type: application/json' \
+     * --data '{
+     * "appName":"flex-thread-pool-test-app",
+     * "threadPoolName": "threadPoolExecutor",
+     * "corePoolSize": 1,
+     * "maximumPoolSize": 10
+     * }'
+     */
+    @PostMapping("update_thread_pool_config")
+    public Response<Boolean> updateThreadPoolConfig(@RequestBody ThreadPoolConfigEntity request) {
+        try {
+            log.info("修改线程池配置开始 {} {} {}", request.getAppName(), request.getThreadPoolName(), JSON.toJSONString(request));
+            RTopic topic = redissonClient.getTopic("FLEX_THREAD_POOL_REDIS_TOPIC" + "_" + request.getAppName());
+            topic.publish(request);
+            log.info("修改线程池配置完成 {} {}", request.getAppName(), request.getThreadPoolName());
+            return Response.<Boolean>builder()
+                    .code(Response.Code.SUCCESS.getCode())
+                    .info(Response.Code.SUCCESS.getInfo())
+                    .data(true)
+                    .build();
+        } catch (Exception e) {
+            log.error("修改线程池配置异常 {}", JSON.toJSONString(request), e);
+            return Response.<Boolean>builder()
+                    .code(Response.Code.UN_ERROR.getCode())
+                    .info(Response.Code.UN_ERROR.getInfo())
+                    .data(false)
+                    .build();
+        }
+    }
+
+}
--- flex-pool-test/src/main/java/com/cong/Application.java
+++ flex-pool-test/src/main/java/com/cong/Application.java
@@ -1,14 +1,54 @@
 package com.cong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @Configuration
 public class Application {
+    public static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(ExecutorService threadPoolExecutor01) {
+        return args -> {
+            while (true){
+                // 创建一个随机时间生成器
+                Random random = new Random();
+                // 随机时间，用于模拟任务启动延迟
+                int initialDelay = random.nextInt(10) + 1; // 1到10秒之间
+                // 随机休眠时间，用于模拟任务执行时间
+                int sleepTime = random.nextInt(10) + 1; // 1到10秒之间
+
+                // 提交任务到线程池
+                threadPoolExecutor01.submit(() -> {
+                    try {
+                        // 模拟任务启动延迟
+                        TimeUnit.SECONDS.sleep(initialDelay);
+                        logger.info("Task started after {} seconds.", initialDelay);
+
+                        // 模拟任务执行
+                        TimeUnit.SECONDS.sleep(sleepTime);
+                        logger.info("Task executed for {} seconds.", sleepTime);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                });
+
+                Thread.sleep(random.nextInt(50) + 1);
+            }
+        };
     }
 }
